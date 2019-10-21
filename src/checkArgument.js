d7f3a43557e2400fa2ffe6fdd088e3c041a75f0d
Gian <gianmarcogh20@gmail.com>
2019-10-21 21:21:22 +0100

    checkArgument

---
+++ src/checkArgument.js
@@ -0,0 +1,14 @@
+// --- EJERCICIO ------------------------------------------------------------------------
+
+// La siguiente situación es muy habitual en JS: al recibir argumentos de una función
+// queremos asegurarnos que no sean "undefined" o nulos. Supon que input es siempre de
+// tipo string, incluyendo null o undefined. Es decir, no vas a recibir números, objetos, etc.
+
+// TIP: Convierte el if/else a un operador ternario. Utiliza también el operador OR.
+
+function f(input) {
+    return input !== undefined ? input === null ? 'Null' : input : 'Undefined';
+};
+console.log(f(undefined));
+console.log(f(null));
+console.log(f('Hola amigos'));
